3c2d23588b97d06bdbdfd880cd1eb18ba992f871
Islom Xujanazarov <islomxujanazarov0501@gmail.com>
2021-06-25 15:31:58 +0500

    reorder file

--- src/component/Registration/RegistrationApplicant.jsx
+++ src/component/Registration/RegistrationApplicant.jsx
@@ -1,29 +1,28 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
 import Title from "../Title";
 import axios from "axios";
-import {toast} from "react-toastify";
-import {withRouter} from 'react-router-dom';
+import { toast } from "react-toastify";
+import { withRouter } from 'react-router-dom';
 
 const RegistrationApplicant = (props) => {
-    const {history} = props;
+    const { history } = props;
     const [regions, setRegions] = useState([]);
     const [districts, setDistricts] = useState([]);
     const [nations, setNations] = useState([]);
     const [socialStatus, setSocialStatus] = useState([]);
     const [values, setValues] = useState(({
         fullName: '',
-        nationId: '',
-        gender: '',
-        birthDate: '',
-        districtId: '',
-        address: '',
-        email: '',
+        sectionId: '',
         phoneNumber: '',
-        socialStatusId: '',
+        email: '',
+        districtId: '',
+        status: true,
         password: '',
+        birthDate: '',
+        address: '',
         prePassword: '',
-        status: false
+        socialStatusId: ''
     }))
 
     useEffect(() => {
@@ -63,7 +62,7 @@ const RegistrationApplicant = (props) => {
 
         if (values.password === values.prePassword) {
             console.log(values)
-            axios.post("/api/auth/createApplicant", {...values}).then(res => {
+            axios.post("/api/auth/createApplicant", { ...values }).then(res => {
                 console.log(res)
                 if (res.data.success) {
                     history.push("/auth/login")
@@ -80,7 +79,7 @@ const RegistrationApplicant = (props) => {
             <div className="registration-applicant container-fluit">
                 <div className="container">
                     <div className="registration-applicant-wrapper">
-                        <Title text="Регистрация"/>
+                        <Title text="Регистрация" />
                         <h5>Анкетные данные</h5>
                         <form onSubmit={handleSend}>
                             <div className="form-wrapper">
@@ -91,7 +90,7 @@ const RegistrationApplicant = (props) => {
                                                 <label className="label" htmlFor="">Ф.И.О</label>
                                                 <input onChange={handleChange} name="fullName" className="input-text"
                                                        type="text"
-                                                       placeholder="Введите ваше Ф.И.О"/>
+                                                       placeholder="Введите ваше Ф.И.О" />
                                             </li>
                                             <li>
                                                 <label className="label" htmlFor="nationId">Национальность</label>
@@ -110,7 +109,6 @@ const RegistrationApplicant = (props) => {
                                                 <div>
                                                     <select id="gender" onChange={handleChange} name="gender"
                                                             className="category">
-                                                        <option value="">Выберите ваш Пол</option>
                                                         <option value="erkak">Erkak</option>
                                                         <option value="ayol">Ayol</option>
                                                     </select>
@@ -119,7 +117,7 @@ const RegistrationApplicant = (props) => {
                                             <li>
                                                 <label className="label" htmlFor="birthDate">Дата рождения</label>
                                                 <input className="input-date" onChange={handleChange} name="birthDate"
-                                                       id="birthDate" type="date"/>
+                                                       id="birthDate" type="date" />
                                             </li>
                                             <li>
                                                 <label className="label" htmlFor="regionId">Область</label>
@@ -157,20 +155,20 @@ const RegistrationApplicant = (props) => {
                                                        id="address"
                                                        className="input-text"
                                                        type="text"
-                                                       placeholder="Введите ваш домашний адрес"/>
+                                                       placeholder="Введите ваш домашний адрес" />
                                             </li>
                                             <li>
                                                 <label className="label" htmlFor="phoneNumber">Телефон</label>
                                                 <input required={true} onChange={handleChange} name="phoneNumber"
                                                        id="phoneNumber"
                                                        className="input-text" type="text"
-                                                       placeholder="+998 (__) ___-__-__"/>
+                                                       placeholder="+998 (__) ___-__-__" />
                                             </li>
                                             <li>
                                                 <label className="label" htmlFor="email">Почта</label>
                                                 <input required={true} onChange={handleChange} name="email" id="email"
                                                        className="input-text" type="text"
-                                                       placeholder="Введите вашу почту"/>
+                                                       placeholder="Введите вашу почту" />
                                             </li>
                                             <li>
                                                 <label className="label" htmlFor="socialStatusId">Категория
@@ -180,8 +178,8 @@ const RegistrationApplicant = (props) => {
                                                             onChange={handleChange}
                                                             className="category">
                                                         <option value="lorem">Выберите льгот</option>
-                                                        {socialStatus && socialStatus.map((item, i) =>
-                                                            <option key={i} value={item.id}>{item.name.uz}</option>
+                                                        {socialStatus && socialStatus.map((item, id) =>
+                                                            <option value={item.id}>{item.name.uz}</option>
                                                         )}
                                                     </select>
                                                 </div>
@@ -191,7 +189,7 @@ const RegistrationApplicant = (props) => {
                                                 <input required={true} onChange={handleChange} name="password"
                                                        id="password"
                                                        className="input-text" type="text"
-                                                       placeholder="Введите вашу почту"/>
+                                                       placeholder="Введите вашу почту" />
                                             </li>
 
                                             <li>
@@ -199,7 +197,7 @@ const RegistrationApplicant = (props) => {
                                                 <input required={true} onChange={handleChange} name="prePassword"
                                                        id="prePassword"
                                                        className="input-text" type="text"
-                                                       placeholder="Повторно вводите пароль"/>
+                                                       placeholder="Повторно вводите пароль" />
                                             </li>
                                         </ul>
                                     </li>
@@ -219,7 +217,7 @@ const RegistrationApplicant = (props) => {
                                         <div className="checked">
 
                                             <input required={true} type="checkbox" id="vehicle1" name="vehicle1"
-                                                   value="Bike"/>
+                                                   value="Bike" />
                                             <label htmlFor="vehicle1"> Я даю согласие на обработку своих персональных
                                                 данных
                                                 и ознакомлен с <a href=""><strong>политикой конфиденциальности</strong></a></label>
@@ -233,7 +231,7 @@ const RegistrationApplicant = (props) => {
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
