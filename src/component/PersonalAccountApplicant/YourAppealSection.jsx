9ae7be1f9a3fa7c3ea51c3fef5e204d9d635afe0
Iskandarbek <iskandarbekergashev@gmail.com>
2021-07-27 11:59:02 +0500

    UserPagination

--- src/component/PersonalAccountApplicant/YourAppealSection.jsx
+++ src/component/PersonalAccountApplicant/YourAppealSection.jsx
@@ -3,21 +3,90 @@ import { API_URL } from "../../utils/constant";
 import axios from "axios";
 import UserAppealItem from "../UserAppealItem";
 import CheckboxConfidensial from "../CheckboxConfidensial";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const YourAppealSection = () => {
 
     const [appeal, setAppeal] = useState([]);
 
+    // pagination \/
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(1);
+
+    const [pageNumberLimit, setPageNumberLimit] = useState(3);
+    const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(3);
+    const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+    const handleClick = (event) => {
+        setCurrentPage(Number(event.target.id));
+    }
+
+    const pages = [];
+    for (let i = 1; i <= Math.ceil(appeal.length / itemsPerPage); i++) {
+        pages.push(i);
+    }
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = appeal.slice(indexOfFirstItem, indexOfLastItem)
+
+    const renderPageNumbers = pages.map(number => {
+        if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+            return (
+                <li
+                    key={number}
+                    id={number}
+                    onClick={handleClick}
+                    className={currentPage == number ? "active" : null}
+                >
+                    {number}
+                </li>
+            )
+        } else {
+            return null;
+        }
+    });
+
+    const handleNextbtn = () => {
+        setCurrentPage(currentPage + 1);
+        if (currentPage + 1 > maxPageNumberLimit) {
+            setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+            setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+        }
+    }
+
+    const handlePrevbtn = () => {
+        setCurrentPage(currentPage - 1);
+        if ((currentPage - 1) % pageNumberLimit == 0) {
+            setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+            setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+        }
+    }
+
+    let pageIncrementBtn = null;
+    if (pages.length > maxPageNumberLimit) {
+        pageIncrementBtn = <li onClick={handleNextbtn}>&hellip;</li>
+    }
+
+    let pageDecrementBtn = null;
+    if (minPageNumberLimit >= 1) {
+        pageDecrementBtn = <li onClick={handlePrevbtn}>&hellip;</li>
+    }
+
+    // pagination /\
+
     useEffect(() => {
         axios.get(API_URL + "/application/myApplications").then(res => {
             setAppeal(res.data.object);
-            console.log(res.data.object)
+            console.log(res.data.object);
         });
     }, []);
 
     return (
         <div className="your-appeal-item-section">
-            {appeal && appeal.map((item, i) =>
+            {appeal && appeal.slice(indexOfFirstItem, indexOfLastItem).map((item, i) =>
                 <div className="content" key={i} value={item.id}>
                     <div className="document-text">
                         <div className="document-text-title">
@@ -49,6 +118,21 @@ const YourAppealSection = () => {
             <div className="new-request">
                 <a href="">Создать новое обращение</a>
             </div>
+            <ul className="page-numbers">
+                <li>
+                    <button disabled={currentPage == pages[0] ? true : false} onClick={handlePrevbtn}>
+                        <ChevronLeftIcon/>
+                    </button>
+                </li>
+                {pageDecrementBtn}
+                {renderPageNumbers}
+                {pageIncrementBtn}
+                <li>
+                    <button onClick={handleNextbtn}>
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+            </ul>
         </div>
     );
 }
--- src/component/PersonalAccountListener/IncomingRequestSection.jsx
+++ src/component/PersonalAccountListener/IncomingRequestSection.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../../utils/constant";
+import axios from "axios";
 import ContentTop from "../ContentTop";
 import IncomingRequestItem from "./IncomingRequestItem";
+import ButtonDefault from "../ButtonDefault";
+import UserName from "../UserName";
+import RequestTheme from "../RequestTheme";
 
 const IncomingRequestSection = () => {
+
+    const [request, setRequest] = useState([]);
+
+    useEffect(() => {
+        axios.get(API_URL + "/application/myApplications").then(res => {
+            setRequest(res.data.object);
+            console.log(res.data);
+        });
+    }, []);
+
     return (
         <div className="incoming-request-section">
             <ContentTop />
-            <IncomingRequestItem/>
+            {request && request.map((item, i) =>
+                <div className="incoming-request-item" key={i} value={item.id}>
+                    <div className="content">
+                        <div className="request-content-title">
+                            <div className="request-content-title-name">
+                                <UserName text="Турсунов Тулкин Мирзаевич" />
+                                <div className="id">id: 12345</div>
+                            </div>
+                            <div className="request-content-title-date">
+                                <div className="date-label">
+                                    Осталось:
+                                </div>
+                                <div className="date-item">
+                                    5 день
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="request-theme">
+                                <div>
+                                    <h3>Тема обращения:<span>{item.title}</span></h3>
+                                </div>
+                                <div>
+                                    <input type="checkbox" />
+                                    <label htmlFor="">Конфиденциально</label>
+                                </div>
+                            </div>
+                            <div className="request-content-item">
+                                <p>{item.description}</p>
+                            </div>
+                        </div>
+                        {/* <RequestTheme /> */}
+                        <div className="category-audio"></div>
+                        <div className="request-bottom">
+                            <a href="">Отправить модератору на замену исполнителя</a>
+                            <a href="">Написать сообщение</a>
+                            <ButtonDefault type="submit" text="Ответить" />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
