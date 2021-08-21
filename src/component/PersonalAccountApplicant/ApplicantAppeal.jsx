011ed00cfe980f773f8750e7732af380bfb8781f
Jonibek <ashirov.uzbek.1997@gmail.com>
2021-08-21 17:21:02 +0500

    aa

--- src/component/PersonalAccountApplicant/ApplicantAppeal.jsx
+++ src/component/PersonalAccountApplicant/ApplicantAppeal.jsx
@@ -8,16 +8,18 @@ import axios from "axios";
 import { API_URL, STORAGE_NAME } from "../../utils/constant";
 import { toast } from "react-toastify";
 import DeleteIcon from '@material-ui/icons/Delete';
+import {CircularProgress} from "@material-ui/core";
 
 const ApplicantAppeal = (props) => {
     const { history } = props;
     const [sections, setSections] = useState([]);
+    const [isLoading,setLoading]=useState(false);
     const [file, setFile] = useState([]);
     const [values, setValues] = useState({
         title: '',
         description: '',
-        sectionId: '',
-        top: '',
+        sectionId: 0,
+        top: false,
         attachmentId: []
     });
     
@@ -26,7 +28,7 @@ const ApplicantAppeal = (props) => {
 
     useEffect(() => {
         axios.get(API_URL + "/section").then(res => {
-            // console.log(res)
+            console.log(res)
             setSections(res.data._embedded.sections);
         })
     }, []);
@@ -34,22 +36,28 @@ const ApplicantAppeal = (props) => {
     const handleChange = (e) => {
         setValues({
             ...values,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.name==="sectionId"?parseInt(e.target.value):e.target.value
         });
     }
 
     const handleSend = (e) => {
         const token = localStorage.getItem(STORAGE_NAME);
         e.preventDefault();
+        let a=new FormData()
+
+        a.append("title",values.title)
+        a.append("sectionId",values.sectionId)
+        a.append("attachmentId",values.attachmentId)
+        a.append("top",values.top)
+        a.append("description",values.description)
         // console.log(values);
         axios({
             url: API_URL + '/application/create',
             method: 'POST',
-            data: {
-                ...values
-            },
+            data: a ,
             headers: {
-                'Authorization': token
+                'Authorization': token,
+                'Content-Type': 'multipart/form-data'
             }
         }).then(res => {
             if (res.data.success) {
@@ -59,10 +67,11 @@ const ApplicantAppeal = (props) => {
                 toast.error(res.data.message);
             }
         });
-        setValues({ ...values, attachmentId: [file] });
+        setValues({ ...values, attachmentId: file });
     }
 
     const handleUpload = (e) => {
+        setLoading(true)
         if (e.target.files[0]) {
             const formData = new FormData();
             formData.append("file", e.target.files[0]);
@@ -75,8 +84,9 @@ const ApplicantAppeal = (props) => {
                 },
                 data: formData
             }).then(res => {
+                    console.log(res)
                 setFile(prevState => [...prevState, res.data.object]);
-                
+                setLoading(false)
             }
             )
         }
@@ -133,20 +143,20 @@ const ApplicantAppeal = (props) => {
                                                 </select>
                                             </div>
                                         </div>
-                                        <div>
-                                            <div className="lb">
-                                                <label className="label" for="">Категория обращения</label>
-                                            </div>
-                                            <div>
-                                                <select onChange={handleChange} id="sectionId" name="sectionId"
-                                                    className="category">
-                                                    <option value="">Выберите ваш обращения</option>
-                                                    {sections && sections.map((item, i) =>
-                                                        <option key={i} value={item.id}>{item.title.uz}</option>
-                                                    )}
-                                                </select>
-                                            </div>
-                                        </div>
+                                        {/*<div>*/}
+                                        {/*    <div className="lb">*/}
+                                        {/*        <label className="label" for="">Категория обращения</label>*/}
+                                        {/*    </div>*/}
+                                        {/*    <div>*/}
+                                        {/*        <select onChange={handleChange} id="sectionId" name="sectionId"*/}
+                                        {/*            className="category">*/}
+                                        {/*            <option value="">Выберите ваш обращения</option>*/}
+                                        {/*            {sections && sections.map((item, i) =>*/}
+                                        {/*                <option key={i} value={item.id}>{item.title.uz}</option>*/}
+                                        {/*            )}*/}
+                                        {/*        </select>*/}
+                                        {/*    </div>*/}
+                                        {/*</div>*/}
                                     </li>
                                     <li>
                                         <div style={{ marginBottom: '20px' }}>
@@ -154,12 +164,9 @@ const ApplicantAppeal = (props) => {
                                                 <label className="label" for="">Прикрепить файл</label>
                                             </div>
                                             <div className="file">
+                                                {isLoading?<CircularProgress color="primary"/>:""}
                                                 <input onChange={handleUpload} type="file" />
                                             </div>
-                                        </div>
-                                        <div className="input-item">
-                                            <input className="input-item-name"></input>
-                                            <DeleteIcon />
                                         </div>
                                     </li>
                                     <li className="confidential">
@@ -169,7 +176,14 @@ const ApplicantAppeal = (props) => {
                                             marginLeft: '60px'
                                         }}>Конфиденциально</label>
                                         <div className="about">
-                                            <input required type="checkbox" id="vehicle1"
+                                            <input required checked={values.top} type="checkbox" onChange={(e)=>{
+                                                console.log(e.target.checked)
+                                                setValues({
+                                                    ...values,
+                                                    top:e.target.checked
+                                                })
+                                            }
+                                            } id="vehicle1"
                                                 name="statusFull" />
                                             <label for="vehicle1">
                                                 данный вопрос не будет отображаться в разделе «Популярные вопросы» в АИС
@@ -179,7 +193,7 @@ const ApplicantAppeal = (props) => {
                                 </ul>
                             </li>
                             <li className="send-button">
-                                <button type="submit" className="btn-default">Отправить</button>
+                                <button type="submit" disabled={isLoading} className="btn-default">Отправить</button>
                             </li>
 
                         </ul>
