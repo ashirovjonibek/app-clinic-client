84dc94b0ab44d9810c990e26963599b2c6f77c7e
Jonibek <ashirov.uzbek.1997@gmail.com>
2021-09-25 22:00:23 +0500

    pasheti complete general process

--- src/component/PersonalAccountSupervisor/SupervisorStatisticSection/Statistics/StatisticsByCategory.js
+++ src/component/PersonalAccountSupervisor/SupervisorStatisticSection/Statistics/StatisticsByCategory.js
@@ -12,6 +12,7 @@ import {
 } from 'recharts';
 import {withTranslation} from "react-i18next";
 import {API_URL, STORAGE_NAME} from "../../../../utils/constant";
+import axios from "axios";
 
 const renderCustomizedLabel = (props) => {
     const {
@@ -25,88 +26,183 @@ const renderCustomizedLabel = (props) => {
 };
 
 function StatisticsByCategory({t}) {
-    const i18 = localStorage.getItem('I18N_LANGUAGE')
+    const i18 = localStorage.getItem('I18N_LANGUAGE');
 
-    const [data, setData] = useState([
-        {name: 'Toshkent shahar', id: 1},
-        {name: 'Toshkent', id: 2},
-        {name: 'Andijon', id: 3},
-        {name: 'Buxoro', id: 4},
-        {name: 'Jizzax', id: 5},
-        {name: 'Qashqa daryo', id: 6},
-        {name: 'Navoiy', id: 7},
-        {name: 'Namangan', id: 8},
-        {name: 'Samarqand', id: 9},
-        {name: 'Surxondaryo', id: 10},
-        {name: 'Sirdaryo', id: 11},
-        {name: 'Farg`ona', id: 12},
-        {name: 'Xorazm', id: 13},
-        {name: 'Qoraqalpo g`iston', id: 14}
-    ]);
-
+    const [data, setData] = useState([]);
     const [section, setSection] = useState([]);
-    const [fetch, setFetch] = useState([]);
+
+    const stringToHslColor=(str, s, l)=> {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let h = hash % 360;
+        return 'hsl('+h+', '+s+'%, '+l+'%)';
+    }
 
     useEffect(() => {
-        fetchData()
-    }, [])
-
-
-    function fetchData() {
-        const axios = require('axios');
-        const config = {
+        axios({
             method: 'get',
             url: API_URL + '/application/filterBySection',
             headers: {
                 'Authorization': localStorage.getItem(STORAGE_NAME)
             }
-        };
-        axios(config)
-            .then(function (response) {
-                setFetch(response.data)
-                fetchSection()
+        })
+            .then(function (res) {
+                let filter = res.data;
+                axios({
+                    method: 'get',
+                    url: API_URL + '/section',
+                    headers: {
+                        'Authorization': localStorage.getItem(STORAGE_NAME)
+                    }
+                })
+                    .then((response) => {
+                        let d = response.data;
+                        let a = [];
+                        axios({
+                            method: 'get',
+                            url: API_URL + '/region'
+                        }).then((response1) => {
+                            let dat = response1.data?._embedded?.regions;
+                            console.log(dat);
+                            filter.map((item1) => {
+                                dat.map((item) => {
+                                    let b = {name: item.name[i18], id: item.id};
+                                    d.map((sec, i) => {
+                                        let kafedra = sec.title[i18];
+                                        let kafedra1 = kafedra.toString();
+                                        let kafedra3 = kafedra1.replace('-', '_');
+                                        let kafedra4 = kafedra3.trim();
+                                        let kafedra5 = kafedra4.replace(' ', '_');
+
+                                        console.log("filter region id", item1.regionId);
+                                        if (item1.regionId === item.id) {
+                                            item1.counts.map((count) => {
+                                                if (count.sectionId === sec.id) {
+                                                    b = {...b, [kafedra5]: count.count}
+                                                }
+                                            })
+                                        }else {
+                                            b = {...b, [kafedra5]: 0}
+                                        }
+                                    });
+                                    a.push(b);
+                                });
+                                console.log("all data------------>", a)
+                            });
+                            setData(a)
+                            setSection(d)
+                        })
+
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             })
             .catch(function (error) {
                 console.log(error);
             });
+    }, []);
+
+
+    function fetchData() {
+        // const axios = require('axios');
+        // const config = {
+        //     method: 'get',
+        //     url: API_URL + '/application/filterBySection',
+        //     headers: {
+        //         'Authorization': localStorage.getItem(STORAGE_NAME)
+        //     }
+        // };
+        // axios(config)
+        //     .then(function (res) {
+        //         console.log(res.data);
+        //         let filter = res.data;
+        //
+        //         const axios = require('axios');
+        //         const config = {
+        //             method: 'get',
+        //             url: API_URL + '/section',
+        //             headers: {
+        //                 'Authorization': localStorage.getItem(STORAGE_NAME)
+        //             }
+        //         };
+        //         axios(config)
+        //             .then(function (response) {
+        //                 {
+        //                     let d = response.data;
+        //                     let a = [];
+        //
+        //                     data.map((item) => {
+        //                         let b = {name: item.name, id: item.id};
+        //
+        //                         d.map((sec, i) => {
+        //                             let kafedra = sec.title[i18];
+        //                             let kafedra1 = kafedra.toString();
+        //                             let kafedra3 = kafedra1.replace('-', '_');
+        //                             let kafedra4 = kafedra3.trim();
+        //                             let kafedra5 = kafedra4.replace(' ', '_');
+        //                             filter.map((item1) => {
+        //                                 item1.counts.map((count) => {
+        //                                     if (count.sectionId === sec.id&&item1.regionId===item.id) {
+        //                                         b = {...b, [kafedra5]: count.count}
+        //                                     }
+        //                                 })
+        //                             });
+        //                         });
+        //                         a.push(b);
+        //                         console.log("all data------------>", a)
+        //                     });
+        //                     setData(a)
+        //                 }
+        //
+        //             })
+        //             .catch(function (error) {
+        //                 console.log(error);
+        //             });
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
     }
 
     function fetchSection() {
-        const axios = require('axios');
-        const config = {
-            method: 'get',
-            url: API_URL + '/section',
-            headers: {
-                'Authorization': localStorage.getItem(STORAGE_NAME)
-            }
-        };
-        axios(config)
-            .then(function (response) {
-                setSection(response.data)
-                {
-                    let d = response.data;
-                    let a = []
-
-                    data.map((item) => {
-                        let b = {region: item.name, id: item.id}
-                        d.map((sec, i) => {
-                            let kafedra = sec.title["uz"]
-                            let kafedra1 = kafedra.toString()
-                            let kafedra3 = kafedra1.replace('-', '_')
-                            let kafedra4 = kafedra3.trim()
-                            let kafedra5 = kafedra4.replace(' ', '_')
-                            b = {...b, [kafedra5]: 0}
-                        })
-                        a.push(b)
-                        console.log("all data------------>", a)
-                    })
-                    setData(a)
-                }
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        // const axios = require('axios');
+        // const config = {
+        //     method: 'get',
+        //     url: API_URL + '/section',
+        //     headers: {
+        //         'Authorization': localStorage.getItem(STORAGE_NAME)
+        //     }
+        // };
+        // axios(config)
+        //     .then(function (response) {
+        //         {
+        //             let d = response.data;
+        //             let a = [];
+        //
+        //             data.map((item) => {
+        //                 let b = {name: item.name, id: item.id};
+        //                 d.map((sec, i) => {
+        //                     let kafedra = sec.title[i18];
+        //                     let kafedra1 = kafedra.toString();
+        //                     let kafedra3 = kafedra1.replace('-', '_');
+        //                     let kafedra4 = kafedra3.trim();
+        //                     let kafedra5 = kafedra4.replace(' ', '_');
+        //                     b = {...b, [kafedra5]: 0}
+        //                 });
+        //                 a.push(b);
+        //                 console.log("all data------------>", a)
+        //             });
+        //             setData(a)
+        //         }
+        //
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
     }
 
     return (
@@ -115,14 +211,14 @@ function StatisticsByCategory({t}) {
                 left: 45
             }} width={400} height={500} data={data} layout="vertical">
                 <CartesianGrid horizontal={false} stroke="#CFD8DC" strokeWidth={0.5}/>
-                <YAxis dataKey={data.region ? "region" : "name"} type="category"/>
+                <YAxis dataKey={"name"} type="category"/>
                 <XAxis type={"number"} tickCount={10} domain={[0, "dataMax+10"]}/>
                 <input type="checkbox"/>
                 <Legend wrapperStyle={{position: 'relative'}}/>
                 <Tooltip itemStyle={{fontSize: 12}} labelStyle={{fontSize: 12}}/>
 
                 {section && section.map((sec, i) => {
-                    let kafedra = sec.title["uz"]
+                    let kafedra = sec.title[i18]
                     let kafedra1 = kafedra.toString()
                     let kafedra3 = kafedra1.replace('-', '_')
                     let kafedra4 = kafedra3.trim()
@@ -131,7 +227,7 @@ function StatisticsByCategory({t}) {
                     return (
 
                         <Bar key={i} barSize={7}
-                             dataKey={kafedra5} fill="#78BAF3">
+                             dataKey={kafedra5} fill={stringToHslColor(kafedra5,50,50)}>
                             {/*<LabelList dataKey={sec.title[i18]}*/}
                             {/*           content={renderCustomizedLabel}/>*/}
                         </Bar>
