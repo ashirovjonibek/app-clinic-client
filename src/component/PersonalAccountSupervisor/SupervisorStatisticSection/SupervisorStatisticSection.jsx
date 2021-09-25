74a70289003e6099500e2ec690ffcd4cf449edff
lordu98 <u.makhmudov211326@gmail.com>
2021-09-25 23:23:43 +0500

    in mobile format statistcs section. fix select option

--- src/component/PersonalAccountSupervisor/SupervisorStatisticSection/SupervisorStatisticSection.jsx
+++ src/component/PersonalAccountSupervisor/SupervisorStatisticSection/SupervisorStatisticSection.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link} from "react-router-dom";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 import ButtonDefault from "../../ButtonDefault";
 import StatisticYearCategories from "./StatisticYearCategories";
 import StatisticGender from "./StatisticGender";
@@ -10,6 +10,7 @@ import StatisticCategories from "./StatisticCategories";
 import StatisticLgots from "./StatisticLgots";
 import StatisticPeriods from "./StatisticPeriods";
 import {withTranslation} from "react-i18next";
+import {get} from "react-hook-form";
 
 const SupervisorStatisticSection = ({t}) => {
 
@@ -18,27 +19,31 @@ const SupervisorStatisticSection = ({t}) => {
     function pushBar(n) {
         switch (n) {
             case 1:
-                return <StatisticRegions />
+                return <StatisticRegions/>
             case 2:
-                return <StatisticYearCategories />
+                return <StatisticYearCategories/>
             case 3:
-                return <StatisticGender />
+                return <StatisticGender/>
             case 4:
-                return <StatisticOverdue />
+                return <StatisticOverdue/>
             case 5:
-                return <StatisticStatus />
+                return <StatisticStatus/>
             case 6:
-                return <StatisticCategories />
+                return <StatisticCategories/>
             case 7:
-                return <StatisticLgots />
+                return <StatisticLgots/>
             case 8:
-                return <StatisticPeriods />
+                return <StatisticPeriods/>
             default:
-                return <StatisticRegions />
+                return <StatisticRegions/>
         }
     }
 
     const getStatistic = (n) => {
+        setStatisticItem(n)
+    }
+    const getStatisticMobile = (e) => {
+        let n = parseInt(e.target.value)
         setStatisticItem(n)
     }
 
@@ -48,7 +53,7 @@ const SupervisorStatisticSection = ({t}) => {
                 <div className="nav-top-buttons">
                     <ul>
                         <li>
-                            <Link to="#" onClick={() => getStatistic(1)} >{t("In the context of regions")}</Link>
+                            <Link to="#" onClick={() => getStatistic(1)}>{t("In the context of regions")}</Link>
                         </li>
                         <li>
                             <Link to="#" onClick={() => getStatistic(2)}>{t("By age categories")}</Link>
@@ -59,10 +64,10 @@ const SupervisorStatisticSection = ({t}) => {
                         {/*<li>*/}
                         {/*    <Link to="#" onClick={() => getStatistic(4)}>{t("Overdue responses")}</Link>*/}
                         {/*</li>*/}
-                    {/*</ul>*/}
-                {/*</div>*/}
-                {/*<div className="nav-top-buttons">*/}
-                {/*    <ul>*/}
+                        {/*</ul>*/}
+                        {/*</div>*/}
+                        {/*<div className="nav-top-buttons">*/}
+                        {/*    <ul>*/}
                         <li>
                             <Link to="#" onClick={() => getStatistic(5)}>{t("By status")}</Link>
                         </li>
@@ -78,26 +83,26 @@ const SupervisorStatisticSection = ({t}) => {
                     </ul>
                 </div>
             </div>
-            <select className="responsive-nav-top" >
-                <option className="responsive-nav-top-show">{t("In the context of regions")}</option>
-                <option className="responsive-nav-top-hidden">{t("By age categories")}</option>
-                <option className="responsive-nav-top-hidden">{t("By gender")}</option>
-                <option className="responsive-nav-top-hidden">{t("Overdue responses")}</option>
-                <option className="responsive-nav-top-hidden">{t("By status")}</option>
-                <option className="responsive-nav-top-hidden">{t("By type of benefits")}</option>
-                <option className="responsive-nav-top-hidden">{t("By type of benefits")}</option>
-                {/*<option className="responsive-nav-top-hidden">{t("For periods")}</option>*/}
+            <select className="responsive-nav-top" onChange={getStatisticMobile}>
+                <option className="responsive-nav-top-show" value="1">{t("In the context of regions")}</option>
+                <option className="responsive-nav-top-hidden" value="2">{t("By age categories")}</option>
+                <option className="responsive-nav-top-hidden" value="3">{t("By gender")}</option>
+                <option className="responsive-nav-top-hidden" value="4">{t("Overdue responses")}</option>
+                <option className="responsive-nav-top-hidden" value="5">{t("By status")}</option>
+                <option className="responsive-nav-top-hidden" value="6">{t("By category")}</option>
+                <option className="responsive-nav-top-hidden" value="7">{t("By type of benefits")}</option>
+                {/*<option className="responsive-nav-top-hidden" value="8">{t("For periods")}</option>*/}
             </select>
             <div className="statistic-section-item">
                 {
                     pushBar(statisticItem)
                 }
             </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '20px 0 10px 0' }}>
-                <ButtonDefault text="Экспорт" />
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '20px 0 10px 0'}}>
+                <ButtonDefault text="Экспорт"/>
             </div>
         </div>
     );
 }
 
-export default withTranslation() (SupervisorStatisticSection);
+export default withTranslation()(SupervisorStatisticSection);
