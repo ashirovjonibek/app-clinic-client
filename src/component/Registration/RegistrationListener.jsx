8ec1b8171d115573c71e8e6805a6a57a2edd5ee7
lordu98 <u.makhmudov211326@gmail.com>
2021-10-04 11:39:59 +0500

    phone number validation

--- src/component/Registration/RegistrationListener.jsx
+++ src/component/Registration/RegistrationListener.jsx
@@ -153,8 +153,17 @@ function RegistrationListener(props) {
 
     const numberHandler = (e) => {
         const name = e.target.name;
-        const regNumber = /^\d+/;
-        if (!regNumber.test(String(e.target.value).toLowerCase()) && name === 'phoneNumber') {
+        const phoneNumber = e.target.value;
+        // const regNumber = /^\d+/;
+        const regNumber = /^((\+)33|0)[1-9](\d{2}){4}$/;
+        // if (!regNumber.test(String(e.target.value).toLowerCase()) && name === 'phoneNumber') {
+        //     setNumberDirty(true);
+        //     setErrorNumber('faqat raqam kiriting!');
+        //     setTimeout(() => {
+        //         e.target.value = ''
+        //     }, 1300)
+        // }
+        if (phoneNumber.match(regNumber) && name === 'phoneNumber') {
             setNumberDirty(true);
             setErrorNumber('faqat raqam kiriting!');
             setTimeout(() => {
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,4 @@
 export const STORAGE_NAME = 'app-clinic';
-export const API_URL = "http://localhost:8080/api";
+// export const API_URL = "http://localhost:8080/api";
+export const API_URL = "https://pro-clinic-server.herokuapp.com/api";
 
